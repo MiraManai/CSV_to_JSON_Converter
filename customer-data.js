073af20f7dcd44a8fd0042244b22c445a376a7ef
mira <amira.manai@esprit.tn>
2017-12-06 16:06:51 +0100

    second commit

--- customer-data.js
+++ customer-data.js
@@ -2,18 +2,19 @@ const csv = require('csvtojson')
 const fs = require('fs')
 const csvFilePath = './customer-data.csv'
 
-var data = ''
+var data = []
 
 csv()
 .fromFile(csvFilePath)
 .on('json', (csvRow) => {
   // creation of json Object
-  data += JSON.stringify(csvRow) + '\n'
+  let chunkOfDate = csvRow
+  data.push(chunkOfDate)
 })
 
 .on('done', (error) => {
   console.log('end')
-  fs.writeFile('./customer-data.json', data, 'utf8', function (err) {
+  fs.writeFile('./customer-data.json', JSON.stringify(data), 'utf8', function (err) {
     if (err) {
       return console.log(err)
     }
